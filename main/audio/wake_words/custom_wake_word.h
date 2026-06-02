a6a0b709ad84c5951b3d1bf2bab1647e05177cbe
ProjectMurzik <deadmiracle@gmail.com>
2026-06-02 23:15:46 +0300

    Update custom_wake_word.h

--- main/audio/wake_words/custom_wake_word.h
+++ main/audio/wake_words/custom_wake_word.h
@@ -40,8 +40,9 @@ private:
     };
 
     // multinet 相关成员变量
-    const esp_afe_iface_t* afe_iface_ = nullptr;
-    esp_afe_data_t* afe_data_ = nullptr;
+    const esp_afe_sr_iface_t* afe_iface_ = nullptr; // Добавили _sr_
+    esp_afe_sr_data_t* afe_data_ = nullptr;         // Добавили _sr_
+
     srmodel_list_t *models_ = nullptr;
     char* mn_name_ = nullptr;
     std::string language_ = "cn";
