ed45415a7bc7f686444cf0fb61c10d4e952d20fa
ProjectMurzik <deadmiracle@gmail.com>
2026-06-02 21:36:34 +0300

    Update audio_service.cc

--- main/audio/audio_service.cc
+++ main/audio/audio_service.cc
@@ -701,15 +701,15 @@ void AudioService::SetModelsList(srmodel_list_t* models_list) {
     models_list_ = models_list;
 
 #if CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32P4
-    if (esp_srmodel_filter(models_list_, ESP_MN_PREFIX, NULL) != nullptr) {
-        wake_word_ = std::make_unique<CustomWakeWord>();
-    } else if (esp_srmodel_filter(models_list_, ESP_WN_PREFIX, NULL) != nullptr) {
+    // В новой версии ESP-SR модели инициализируются через afe_config_init
+    // WakeWord будет создаваться через AFE
+    if (models_list_ != nullptr) {
         wake_word_ = std::make_unique<AfeWakeWord>();
     } else {
         wake_word_ = nullptr;
     }
 #else
-    if (esp_srmodel_filter(models_list_, ESP_WN_PREFIX, NULL) != nullptr) {
+    if (models_list_ != nullptr && esp_srmodel_filter(models_list_, ESP_WN_PREFIX, NULL) != nullptr) {
         wake_word_ = std::make_unique<EspWakeWord>();
     } else {
         wake_word_ = nullptr;
